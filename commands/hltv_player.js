dcd0cd7935d13826f72c639d69dcf174c454cc6d
John Sun <jsun.1207@gmail.com>
2019-05-21 20:37:25 -0700

    hltv_player command support searching by player name

--- commands/hltv_player.js
+++ commands/hltv_player.js
@@ -1,33 +1,48 @@
 const Discord = require('discord.js');
+const request = require('request');
 const { HLTV } = require('hltv');
 
 exports.run = async(client, message, args) => {
-    let getPlayerPromise = HLTV.getPlayer({id: args[0]});
-    
-    getPlayerPromise.then((player) => {
-        console.log(player);
+    request(`${HLTV.config.hltvUrl}/search?term=${args[0]}`, (err, res, body) => {
+        if (err) {
+            message.channel.send('Problem searching for player.');
+            console.log(err);
+            return;
+        }
 
-        let playerEmbed = new Discord.RichEmbed()
-                        .setColor('#0099ff')
-                        .setTitle(player.ign)
-                        .setURL(`https://www.hltv.org/stats/players/${args[0]}/${player.ign}`)
-                        .setDescription(player.name)
-                        .setThumbnail(player.image)
-                        .addField('Twitter', player.twitter)
-                        .addField('Twitch', player.twitch)
-                        .addBlankField()
-                        .addField('Rating', player.statistics.rating)
-                        .addField('Kills Per Round', player.statistics.killsPerRound)
-                        .addField('Headshots', player.statistics.headshots)
-                        .addField('Maps Played', player.statistics.mapsPlayed)
-                        .addField('Deaths Per Round', player.statistics.deathsPerRound)
-                        .addField('Round Contributed', player.statistics.roundsContributed);
+        var searchObj = JSON.parse(body);
+        if (searchObj[0].players[0] === undefined) {
+            message.channel.send("Player doesn't exist.");
+            return;
+        }
 
-        message.channel.send(playerEmbed);
-    });
+        let playerId = searchObj[0].players[0].id;
 
-    getPlayerPromise.catch((err) => {
-        console.log(err);
-        message.channel.send('Error fetching player data.');
+        let getPlayerPromise = HLTV.getPlayer({id: playerId});
+
+        getPlayerPromise.then((player) => {
+            let playerEmbed = new Discord.RichEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(player.ign)
+                            .setURL(`https://www.hltv.org/stats/players/${args[0]}/${player.ign}`)
+                            .setDescription(player.name)
+                            .setThumbnail(player.image)
+                            .addField('Twitter', player.twitter)
+                            .addField('Twitch', player.twitch)
+                            .addBlankField()
+                            .addField('Rating', player.statistics.rating)
+                            .addField('Kills Per Round', player.statistics.killsPerRound)
+                            .addField('Headshots', player.statistics.headshots)
+                            .addField('Maps Played', player.statistics.mapsPlayed)
+                            .addField('Deaths Per Round', player.statistics.deathsPerRound)
+                            .addField('Round Contributed', player.statistics.roundsContributed);
+
+            message.channel.send(playerEmbed);
+        });
+
+        getPlayerPromise.catch((err) => {
+            console.log(err);
+            message.channel.send('Error fetching player data.');
+        });
     });
 }
