9bc9657b0cdad8cd4b06d8bcd289a3800249bbe9
John Sun <jsun.1207@gmail.com>
2019-07-07 18:08:05 -0700

    cache results for player and team profiles

--- commands/hltv_player.js
+++ commands/hltv_player.js
@@ -1,49 +1,66 @@
 const Discord = require('discord.js');
 const request = require('request');
 const { HLTV } = require('hltv');
+const NodeCache = require('node-cache');
+
+const playerCache = new NodeCache({ stdTTL: 43200, checkperiod: 10800 });
 
 exports.run = async(client, message, args) => {
-    request(`${HLTV.config.hltvUrl}/search?term=${args[0]}`, (err, res, body) => {
-        if (err) {
-            message.channel.send('Problem searching for player.');
-            console.log(err);
-            return;
-        }
+    let cachedPlayer = playerCache.get(args[0]);
 
-        var searchObj = JSON.parse(body);
-        if (searchObj[0].players[0] === undefined) {
-            message.channel.send("Player doesn't exist.");
-            return;
-        }
-
-        let playerId = searchObj[0].players[0].id;
-
-        let getPlayerPromise = HLTV.getPlayer({id: playerId});
-
-        getPlayerPromise.then((player) => {
-            let playerEmbed = new Discord.RichEmbed()
-                              .setColor('#0099ff')
-                              .setTitle(player.ign)
-                              .setURL(`https://www.hltv.org/stats/players/${playerId}/${player.ign}`)
-                              .setDescription(player.name)
-                              .setThumbnail(player.image)
-                              .addField('Twitter', player.twitter)
-                              .addField('Twitch', player.twitch)
-                              .addBlankField()
-                              .addField('Stats For Past 3 Months', '\u200b')
-                              .addField('Rating', player.statistics.rating)
-                              .addField('Kills Per Round', player.statistics.killsPerRound)
-                              .addField('Headshots', player.statistics.headshots)
-                              .addField('Maps Played', player.statistics.mapsPlayed)
-                              .addField('Deaths Per Round', player.statistics.deathsPerRound)
-                              .addField('Round Contributed', player.statistics.roundsContributed);
-
-            message.channel.send(playerEmbed);
+    if (cachedPlayer != undefined) {
+        console.log("RETURNING CACHED PLAYER PROFILE");
+        sendPlayer(message, cachedPlayer);
+    }
+    else {
+        request(`${HLTV.config.hltvUrl}/search?term=${args[0]}`, (err, res, body) => {
+            if (err) {
+                message.channel.send('Problem searching for player.');
+                console.log(err);
+                return;
+            }
+    
+            var searchObj = JSON.parse(body);
+            if (searchObj[0].players[0] === undefined) {
+                message.channel.send("Player doesn't exist.");
+                return;
+            }
+    
+            let playerId = searchObj[0].players[0].id;
+    
+            let getPlayerPromise = HLTV.getPlayer({id: playerId});
+    
+            getPlayerPromise.then((player) => {
+                // don't care about cache success or fail for now
+                playerCache.set(args[0], player);
+                sendPlayer(message, player);
+            });
+    
+            getPlayerPromise.catch((err) => {
+                console.log(err);
+                message.channel.send('Error fetching player data.');
+            });
         });
+    }
+}
 
-        getPlayerPromise.catch((err) => {
-            console.log(err);
-            message.channel.send('Error fetching player data.');
-        });
-    });
+var sendPlayer = (message, player) => {
+    let playerEmbed = new Discord.RichEmbed()
+                                  .setColor('#0099ff')
+                                  .setTitle(player.ign)
+                                  .setURL(`https://www.hltv.org/stats/players/${player.id}/${player.ign}`)
+                                  .setDescription(player.name)
+                                  .setThumbnail(player.image)
+                                  .addField('Twitter', player.twitter)
+                                  .addField('Twitch', player.twitch)
+                                  .addBlankField()
+                                  .addField('Stats For Past 3 Months', '\u200b')
+                                  .addField('Rating', player.statistics.rating)
+                                  .addField('Kills Per Round', player.statistics.killsPerRound)
+                                  .addField('Headshots', player.statistics.headshots)
+                                  .addField('Maps Played', player.statistics.mapsPlayed)
+                                  .addField('Deaths Per Round', player.statistics.deathsPerRound)
+                                  .addField('Round Contributed', player.statistics.roundsContributed);
+    
+    message.channel.send(playerEmbed);
 }
--- commands/hltv_team.js
+++ commands/hltv_team.js
@@ -1,30 +1,54 @@
 const Discord = require('discord.js');
 const request = require('request');
 const { HLTV } = require('hltv');
+const NodeCache = require('node-cache');
+
+const teamCache = new NodeCache({ stdTTL: 43200, checkperiod: 10800 });
 
 exports.run = async(client, message, args) => {
-    request(`${HLTV.config.hltvUrl}/search?term=${args[0]}`, (err, res, body) => {
-        if (err) {
-            message.channel.send('Problem searching for team.');
-            console.log(err);
-            return;
-        }
+    let cachedTeam = teamCache.get(args[0]);
 
-        var searchObj = JSON.parse(body);
-        if (searchObj[0].teams[0] === undefined) {
-            message.channel.send("Team doesn't exist.");
-            return;
-        }
+    if (cachedTeam != undefined) {
+        console.log("RETURNING CACHED TEAM PROFILE");
+        sendTeam(message, cachedTeam);
+    }
+    else {
+        request(`${HLTV.config.hltvUrl}/search?term=${args[0]}`, (err, res, body) => {
+            if (err) {
+                message.channel.send('Problem searching for team.');
+                console.log(err);
+                return;
+            }
+    
+            var searchObj = JSON.parse(body);
+            if (searchObj[0].teams[0] === undefined) {
+                message.channel.send("Team doesn't exist.");
+                return;
+            }
+    
+            let teamId = searchObj[0].teams[0].id;
+    
+            let getTeamPromise = HLTV.getTeam({id: teamId});
+    
+            getTeamPromise.then((team) => {
+                // don't care about cache success or fail for now
+                teamCache.set(args[0], team);
+                sendTeam(message, team);
+            });
+    
+            getTeamPromise.catch((err) => {
+                console.log(err);
+                message.channel.send('Error fetching team data.');
+            });
+        });
+    }
+}
 
-        let teamId = searchObj[0].teams[0].id;
-
-        let getTeamPromise = HLTV.getTeam({id: teamId});
-
-        getTeamPromise.then((team) => {
-            let teamEmbed = new Discord.RichEmbed()
+var sendTeam = (message, team) => {
+    let teamEmbed = new Discord.RichEmbed()
                               .setColor('#0099ff')
                               .setTitle(team.name)
-                              .setURL(`https://www.hltv.org/team/${teamId}/${team.name.toLowerCase().replace(/ /g, '-')}`)
+                              .setURL(`https://www.hltv.org/team/${team.id}/${team.name.toLowerCase().replace(/ /g, '-')}`)
                               .setDescription(team.location)
                               .setThumbnail(team.logo)
                               .addField('Rank', team.rank)
@@ -42,12 +66,5 @@ exports.run = async(client, message, args) => {
                 teamEmbed.addField(`${team.name}   ${matchResult.result}   ${matchResult.enemyTeam.name}`, matchResult.event.name);
             });
 
-            message.channel.send(teamEmbed);
-        });
-
-        getTeamPromise.catch((err) => {
-            console.log(err);
-            message.channel.send('Error fetching team data.');
-        });
-    });
+    message.channel.send(teamEmbed);
 }
